c2c744a8f01488c38140a5a239f9b72018bf4e56
Amina Pansher <amina.pansher@yandex.ru>
2025-04-26 19:59:56 +0300

    Добавлен вывод статей

--- articles.php
+++ articles.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Список статей</title>
+    <style>
+        .article {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 10px;
+        }
+    </style>
+</head>
+<body>
+    <h1>Список статей</h1>
 <?php
 include 'includes/db.php';
 $result = pg_query($conn, "SELECT * FROM articles ORDER BY created_at DESC");
@@ -9,4 +23,6 @@ while ($row = pg_fetch_assoc($result)) {
          </div>";
 }
 ?>
+</body>
+</html>
 
